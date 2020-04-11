550b0efef8bc1cc147c90b45c52480dd4c472f39
Benjamin Doggett <bbdoggett3@gmail.com>
2020-04-10 22:44:32 -0600

    Created a JS file in order to have more practice

---
+++ Precourse-HandsOn/code.js
@@ -0,0 +1,22 @@
+    /* ********************************************** 
+    * I wrote the same code inline in my HTML file 
+    * for some extra practice in understanding these 
+    * variables and the declaration tools in working
+    * with them
+    * ***********************************************/    
+    
+    //KEYWORD LABEL = VALUE
+    var name = "Benjamin";
+
+    //KEYWORD LABEL -- this is the declaration
+    //LABEL = VALUE -- this is the assignment hence the equal sign.
+    var email;
+    email = "bendog@gmail.com";
+
+    //let and const are like var but more specific.
+
+    const hello = "Hello"; //this cannot be changed.
+    hello = "World";
+
+    //Both of these forms of declaring and assigning a value are
+    //used in programming and either on is ok.
