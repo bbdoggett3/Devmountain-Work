465427d562bb403f890aafa68cf40a8f727d0c89
Benjamin Doggett <bbdoggett3@gmail.com>
2020-04-10 22:56:05 -0600

    Made some fun variables as more practice.

--- Precourse-HandsOn/code.js
+++ Precourse-HandsOn/code.js
@@ -2,7 +2,9 @@
     * I wrote the same code inline in my HTML file 
     * for some extra practice in understanding these 
     * variables and the declaration tools in working
-    * with them
+    * with them. I did some of my own practice on this
+    * section of the precourse material in order to feel
+    * comfortable working with JS.
     * ***********************************************/    
     
     //KEYWORD LABEL = VALUE
@@ -13,13 +15,20 @@
     var email;
     email = "bendog@gmail.com";
 
+    //Both of these forms of declaring and assigning a value are
+    //used in programming and either on is ok.
+
     //let and const are like var but more specific.
 
-    const hello = "Hello"; //this cannot be changed.
-    hello = "World"; //This will throw an error
+    const hello = "Hello, your name is"; //this cannot be changed.
+    /*hello = "World";*/
+    //This will throw an error
     // When you are debugging make sure to look for the word
     //"TYPEERROR" in the console, this will give you a start
     // to debugging.
 
-    //Both of these forms of declaring and assigning a value are
-    //used in programming and either on is ok.
+    console.log(hello, name + " and your email is " + email); // here we are accessing
+    //the value that is inside our "make-believe-box."
+
+    let age = 999;
+    console.log("You are " + age + " years old.");
