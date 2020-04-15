0bfc5e65d83697e8a78d7673ec4c1b916de96cb5
Benjamin Doggett <bbdoggett3@gmail.com>
2020-04-15 16:09:33 -0600

    learned some built in methods for arrays.

--- Precourse-HandsOn/arrays.js
+++ Precourse-HandsOn/arrays.js
@@ -1,4 +1,16 @@
 //This will hold my practical skills involved with arrays
 
 //What is an array? And how to create one
-let myFriends = ['Jeremy', 'Missy', 'Break'];   //this is the syntax of an array
+let myFriends = ['Jeremy', 'Missy', 'Brek'];   //this is the syntax of an array
+let singleFriend = myFriends[1]; // you can look up a friend at any index from 0-2
+
+//You can use push, pop, shift, unshift, splice, slice
+myFriends.push('Brian'); // adds to the end of the array
+myFriends.pop(); // removes friend at the end of the array
+myFriends.shift(); //removes at the front of the array
+myFriends.unshift('Jeremy'); //Add someone to the front of the array
+myFriends.splice(0, 1) // start the splicing (removing) at index 0 and only move 1 space
+let copy= myFriends.slice(1, 2) //started copying the array at index 1 (missy) and up to index 2 but not included
+
+
+
