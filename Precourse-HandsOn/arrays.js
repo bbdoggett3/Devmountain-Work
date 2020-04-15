a80e37a4d589595615d610e6a672e9e5b26c262b
Benjamin Doggett <bbdoggett3@gmail.com>
2020-04-15 15:54:56 -0600

    wrote the syntax for an array

--- Precourse-HandsOn/arrays.js
+++ Precourse-HandsOn/arrays.js
@@ -1,1 +1,4 @@
 //This will hold my practical skills involved with arrays
+
+//What is an array? And how to create one
+let myFriends = ['Jeremy', 'Missy', 'Break'];   //this is the syntax of an array
