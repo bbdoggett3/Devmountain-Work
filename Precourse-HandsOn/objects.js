2a7aed0af00ae8814849264dce4081030049c6e7
Benjamin Doggett <bbdoggett3@gmail.com>
2020-04-17 09:48:25 -0600

    Created an object to hold person info.

--- Precourse-HandsOn/objects.js
+++ Precourse-HandsOn/objects.js
@@ -1,1 +1,10 @@
 //Here is my objects practice from the pre-course
+
+//This is a object
+var person = {
+    name: "Joe Dirt",   //name here is the key, and 'Joe Dirt' is the value.
+    state: "Alabama",
+    haircut: "Mullet"
+}
+
+console.log("Hello, " + person.name + ". You are from " + person.state + ". I like your " + person.haircut);
