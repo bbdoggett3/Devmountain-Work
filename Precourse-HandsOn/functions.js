4807609fd99d16793dedf9efb181984c2a23b088
Benjamin Doggett <bbdoggett3@gmail.com>
2020-04-17 10:36:16 -0600

    practiced defining a function in 3 formats

--- Precourse-HandsOn/functions.js
+++ Precourse-HandsOn/functions.js
@@ -1,1 +1,20 @@
 //Here is my examples/practice with functions
+//What is a funtion?
+//How to define a funtion? Declaration vs expression vs arrow?
+//How to invoke a function?
+//What is a return statement?
+
+//Function Delcaration 
+//function createVar() {
+//    let myName = "Ben";
+//}
+
+//Function Expression 
+//let createVar = function() {
+//    let myName = "Ben";
+//}
+
+//Function Arrow
+//let createVar = () => {
+//    let myName = "Ben";
+//}
