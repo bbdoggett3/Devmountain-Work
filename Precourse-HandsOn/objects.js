b0030fc2623f9528827f2679e1245fa3fd98f663
Benjamin Doggett <bbdoggett3@gmail.com>
2020-04-17 09:56:35 -0600

    Added a new object into my JS

--- Precourse-HandsOn/objects.js
+++ Precourse-HandsOn/objects.js
@@ -4,7 +4,16 @@
 var person = {
     name: "Joe Dirt",   //name here is the key, and 'Joe Dirt' is the value.
     state: "Alabama",
-    haircut: "Mullet"
+    haircut: "Mullet",
+    age: 32
+}
+
+var person2 = {
+    name: "Gandalf",
+    state: "Middle Earth",
+    haircut: "long",
+    age: "unknown"
 }
 
 console.log("Hello, " + person.name + ". You are from " + person.state + ". I like your " + person.haircut);
+console.log(person.name + " is " + person.age +" years old.");
