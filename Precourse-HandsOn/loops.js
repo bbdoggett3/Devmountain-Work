e8c6afddd05df3309440eef3c5730626e236ed0a
Benjamin Doggett <37028720+bbdoggett3@users.noreply.github.com>
2020-04-21 23:17:33 -0600

    Create loops.js

---
+++ Precourse-HandsOn/loops.js
@@ -0,0 +1,20 @@
+//This is my file for loops
+
+//EXAMPLE OF A FOUR LOOP
+for(SETUP; CONDITION; ITERATION) {
+
+}
+
+for(var i=0; i < 10; i++) {
+   console.log(i) 
+}
+
+//EXAMPLE OF A WHILE LOOP
+while(CONDITION) {
+    ITERATION
+}
+var j = 0;
+while(j < 10) {
+    console.log("J", j)
+    j++;
+}
